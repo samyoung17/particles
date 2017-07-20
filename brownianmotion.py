87c8e057b0538a55bcc356e3f03b3cc42aa14e2e
sam <samyoung17a@gmail.com>
2017-07-20 14:46:34 +0100

    Weak limit Langevin Equation

--- brownianmotion.py
+++ brownianmotion.py
@@ -1,20 +1,25 @@
 import numpy as np
 import particlesim
 
+M = 1
+GAMMA = 0
+
 def moveParticles(particles, t):
 	cov = [[np.sqrt(t), 0], [0, np.sqrt(t)]]
 	mean = (0, 0)
-	dx = np.random.multivariate_normal(mean, cov, len(particles))
+	xi = np.random.multivariate_normal(mean, cov, len(particles))
 	for i, particle in enumerate(particles):
 		x0 = particle.x
-		x = x0 + dx[i]
-		v = dx[i] / t
+		v0 = particle.v
+		a = - GAMMA / M + (1/M) * xi[i]
+		v = v0 + a
+		x = x0 + (v + v0)/2 * t
 		particle.x = x
 		particle.v = v
 
 def main():
 	data = particlesim.simulate(1000, 200, moveParticles)
-	particlesim.motionAnimation(data, 100, 100)
+	particlesim.motionAnimation(data, 100, 1000)
 
 if __name__=='__main__':
 	main()
