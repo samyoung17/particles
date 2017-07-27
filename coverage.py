fc5f513e0a79e1199039dde18cc42d5e69fb14ae
sam <samyoung17a@gmail.com>
2017-07-27 14:44:46 +0100

    Replacing average min distance with sup min distance

--- coverage.py
+++ coverage.py
@@ -11,14 +11,14 @@ N = 50
 def nearestNeighbour(y, particles):
 	return np.min(map(lambda x: np.linalg.norm(x - y), particles))
 
-def avgMinDistance(particles, targets):
+def supMinDistance(particles, targets):
 	distances = map(lambda y: nearestNeighbour(y, particles), targets)
-	return np.mean(distances)
+	return np.max(distances)
 
-def avgMinDistanceOverTime(data):
+def supMinDistanceOverTime(data):
 	d = np.zeros((data.iterations))
 	for i in range(data.iterations):
-		d[i] = avgMinDistance(data.x[i], data.y[i])
+		d[i] = supMinDistance(data.x[i], data.y[i])
 	return d
 
 def main():
@@ -28,17 +28,17 @@ def main():
 	fdData = particlesim.simulate(ITERATIONS, N, forcedistribution.moveParticles)
 	print('Simulating Run and Tumble')
 	rtData = particlesim.simulate(ITERATIONS, N, runtumble.moveParticles)
-	print('Calculating Avg Min Distances Brownian Motion')
-	bmDistances = avgMinDistanceOverTime(bmData)
-	print('Calculating Avg Min Distances Force Distribution')
-	fdDistances = avgMinDistanceOverTime(fdData)
-	print('Calculating Avg Min Distances Run And Tumble')
-	rtDistances = avgMinDistanceOverTime(rtData)
+	print('Calculating Sup Min Distances Brownian Motion')
+	bmDistances = supMinDistanceOverTime(bmData)
+	print('Calculating Sup Min Distances Force Distribution')
+	fdDistances = supMinDistanceOverTime(fdData)
+	print('Calculating Sup Min Distances Run And Tumble')
+	rtDistances = supMinDistanceOverTime(rtData)
 	bmPlot, = plt.plot(bmDistances, label = 'BM')
 	fdPlot, = plt.plot(fdDistances, label = 'FD')
 	rtPlot, = plt.plot(rtDistances, label = 'RT')
 	plt.legend(handles=[bmPlot, fdPlot, rtPlot])
-	plt.title('Average distance to from target to nearest particle')
+	plt.title('Maximum distance to from target to nearest particle')
 	plt.show()
 
 if __name__=='__main__':
