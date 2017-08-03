e3678c75d74bf70e12175d6d65b257bc0217a9d5
sam <samyoung17a@gmail.com>
2017-08-03 11:41:09 +0100

    Reintroducing weak langevin

---
+++ langevin.py
@@ -0,0 +1,34 @@
+import numpy as np
+import particlesim
+import hardboundary
+import matplotlib.pyplot as plt
+
+M = 1
+GAMMA = 0.01
+D = 0.1
+
+def moveParticles(particles, t):
+	cov = [[t, 0], [0, t]]
+	mean = (0, 0)
+	xi = D * np.random.multivariate_normal(mean, cov, len(particles))
+	for i, particle in enumerate(particles):
+		x0, v0 = particle.x, particle.v
+		a = - v0 * GAMMA / M + (1/M) * xi[i]
+		v = v0 + a
+		x = x0 + (v + v0)/2 * t
+		x,v = hardboundary.bounceIfHitsBoundary(x, v, t, particlesim.R_MAX)
+		particle.x, particle.v = x, v
+
+def main():
+	data = particlesim.simulate(5000, 50, moveParticles)
+	particlesim.motionAnimation(data, 100)
+
+def averageSpeed():
+	data = particlesim.simulate(100000, 50, moveParticles)
+	s = np.linalg.norm(data.v, axis=2)
+	sbar = s.mean(axis=1)
+	plt.plot(sbar)
+	plt.show()
+
+if __name__=='__main__':
+	main()
