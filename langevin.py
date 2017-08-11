Further fixes to Langevin method calibration, adding missed time dependent term on drag coefficient, fixing incorrect Temperature and brownian variance corrections

langevin.py
@@ -2,24 +2,22 @@ import numpy as np
 import particlesim
 import hardboundary
 import matplotlib.pyplot as plt
-import linalgutil
 
 M = 1.0
 GAMMA = 0.1
 S = 0.2
-T = 4 * M * pow(S,2) / np.pi
+T = 2 * M * pow(S,2) / np.pi
 
 
 def moveParticles(particles, t):
 	var = 2 * GAMMA * T * t
 	cov = [[var, 0], [0, var]]
 	mean = (0, 0)
-	b = np.random.multivariate_normal(mean, cov, len(particles)) \
-		  * linalgutil.expectedNormMultivariateGaussian(1) # Shouldn't this be a division???
+	b = np.random.multivariate_normal(mean, cov, len(particles))
 	for i, particle in enumerate(particles):
 		x0, v0 = particle.x, particle.v
-		a = - v0 * GAMMA / M + (1/M) * b[i]
-		v = v0 + a
+		dv = - v0 * GAMMA / M * t + (1/M) * b[i]
+		v = v0 + dv
 		x = x0 + (v + v0)/2 * t
 		x,v = hardboundary.bounceIfHitsBoundary(x, v, t, particlesim.R_MAX)
 		particle.x, particle.v = x, v
@@ -37,7 +35,7 @@ def averageSpeedAndTemp():
 	sbar = s.mean(axis=1)
 	plt.plot(sbar)
 	plt.show()
-	e = np.apply_along_axis(lambda v: M * pow(np.linalg.norm(v), 2), 2, data.v)
+	e = np.apply_along_axis(lambda v: 0.5 * M * pow(np.linalg.norm(v), 2), 2, data.v)
 	ebar = e.mean(axis=1)
 	plt.plot(ebar)
 	plt.show()