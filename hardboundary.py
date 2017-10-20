b06ceeed83952f89057b7b24e58251f16501c227
sam <samyoung17a@gmail.com>
2017-10-20 17:22:14 +0100

    Removing duplicate functions

--- hardboundary.py
+++ hardboundary.py
@@ -1,26 +1,18 @@
 import numpy as np
-
-def polarToCart(z):
-	r, theta = z
-	return np.array((r * np.sin(theta), r * np.cos(theta)))
-
-def cartToPolar(y):
-	theta = np.arctan2(y[0], y[1])
-	r = np.linalg.norm(y,2)
-	return np.array((r, theta))
+import linalgutil as la
 
 def projectToBoundary(x, rMax):
-	r, phi = cartToPolar(x)
-	return polarToCart((rMax, phi))
+	r, phi = la.cartToPolar(x)
+	return la.polarToCart((rMax, phi))
 
 def angleBetweenTwoVectors(a, b):
 	return np.arccos(np.dot(a,b) / (np.linalg.norm(a,2) * np.linalg.norm(b,2)))
 
 def bounce(x, v, t, rMax):
 	theta = angleBetweenTwoVectors(x, v)
-	r, phi = cartToPolar(x)
+	r, phi = la.cartToPolar(x)
 	psi = np.pi + phi - (2 * theta)
-	vPrime = polarToCart((np.linalg.norm(v,2), psi))
+	vPrime = la.polarToCart((np.linalg.norm(v,2), psi))
 	xPrime = projectToBoundary(x, rMax) + vPrime * t
 	return xPrime, vPrime
 
