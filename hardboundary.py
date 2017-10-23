47723ddfdec10b526dac3c2dacd1b3fa04f7a555
sam <samyoung17a@gmail.com>
2017-10-23 14:51:02 +0100

    Removing redundant function

--- hardboundary.py
+++ hardboundary.py
@@ -5,11 +5,8 @@ def projectToBoundary(x, rMax):
 	r, phi = la.cartToPolar(x)
 	return la.polarToCart((rMax, phi))
 
-def angleBetweenTwoVectors(a, b):
-	return np.arccos(np.dot(a,b) / (np.linalg.norm(a,2) * np.linalg.norm(b,2)))
-
 def bounce(x, v, t, rMax):
-	theta = angleBetweenTwoVectors(x, v)
+	theta = la.angleBetweenTwoVectors(x, v)
 	r, phi = la.cartToPolar(x)
 	psi = np.pi + phi - (2 * theta)
 	vPrime = la.polarToCart((np.linalg.norm(v,2), psi))
