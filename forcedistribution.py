03e88831271db2ed4bf1570b0c56b1a150e192ff
sam <samyoung17a@gmail.com>
2017-11-21 15:59:37 +0000

    Prevent the particles from jumping across the boundary in between timesteps

--- forcedistribution.py
+++ forcedistribution.py
@@ -5,6 +5,8 @@ import numpy as np
 # BOUNDARY = electrostaticboundary.Circle(particlesim.R_MAX)
 BOUNDARY = electrostaticboundary.Square(2 * particlesim.R_MAX)
 
+EPSILON = 0.0001
+
 K_E  = 1
 NU = 0.02
 M = 1
@@ -31,14 +33,17 @@ def moveParticles(particles, t, boundary):
 		Fd = forceDueToDragUnitConstants(v0, M)
 		a = (F + Fb + Fd) / M
 		v = a * t + v0
-		x = x0 + (v + v0) / 2
+		x = x0 + (v + v0) * t / 2
+		# Prevent the particles from jumping across the boundary in between timesteps
+		while not boundary.contains(x * (1 + EPSILON)):
+			x = (x0 + x) / 2
 		particle.x, particle.v, particle.F, particle.Fd = x, v, F, Fd
 
 def main():
-	n, iterations = 100, 1000
+	n, iterations = 100, 2000
 	folder = 'data/electrostatic n={} iter={}'.format(n, iterations)
 	data = particlesim.simulate(iterations, n, moveParticles, folder, BOUNDARY)
-	particlesim.motionAnimation(data, 15, BOUNDARY)
+	particlesim.motionAnimation(data, 20, BOUNDARY)
 
 if __name__ == '__main__':
 	main()
