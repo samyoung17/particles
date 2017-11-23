b11755c0e4f4aae1c711a46aa33bc50e2ae7841c
sam <samyoung17a@gmail.com>
2017-11-23 16:31:31 +0000

    Refactor voronoi to extract circular boundary

--- voronoi.py
+++ voronoi.py
@@ -3,9 +3,13 @@ import particlesim
 import scipy.spatial
 import matplotlib.pyplot as plt
 import linalgutil as la
+import voronoiboundary
 
 K_PROP = 1
 S_MAX = 0.3
+
+
+BOUNDARY = voronoiboundary.Circle(particlesim.R_MAX)
 
 class VoronoiCell:
 	def __init__(self, point, vertices=None, centroid=None):
@@ -35,62 +39,12 @@ def createRidgeDict(voronoi):
 		all_ridges.setdefault(p2, []).append((p1, v1, v2))
 	return  all_ridges
 
-def findIntersectionWithCircle(v, midpoint):
-	'''
-	We use the geometry of a triangle with points (origin, midpoint, new boundary vertex)
-	to find the vector to the new boundary vertex in the voronoi diagram.
-	create a unit vector pointing from the vertex to the nearest boundary through the midpoint of x1 and x2
-		alpha is the angle between the midpoint vector and the vector from the midpoint to the new boundary vertex
-		beta is the angle between the midpoint/boundary vertex vector and the boundary vertex vector
-		gamma is the angle between the boundary vertex vector and the midpoint vector
-	'''
-	ridgeUnit = (midpoint - v) / np.linalg.norm(midpoint - v)
-	if np.dot(midpoint, ridgeUnit) < 0:
-		ridgeUnit = -ridgeUnit
-	r, theta = la.cartToPolar(v)
-	s, phi = la.cartToPolar(ridgeUnit)
-	alpha = np.pi - theta + phi
-	b = np.linalg.norm(midpoint)
-	a = particlesim.R_MAX
-	# Apply the sine rule once to find beta
-	beta = np.arcsin(b * np.sin(alpha) / a)
-	gamma = np.pi - alpha - beta
-	# Apply the sine rule a second time to find the length of the midpoint/boundary vertex vector
-	c = np.sin(gamma) / np.sin(beta) * b
-	# Add the midpiont/boundary vertex vector to the midpoint vector to yield the new vertex vector
-	intersectingPoint = la.polarToCart((c, phi)) + midpoint
-	return intersectingPoint
-
 def findRegionVertices(voronoi, p1, ridges):
 	regionVertexIndices = voronoi.regions[voronoi.point_region[p1]]
 	originalVertices = map(lambda i: voronoi.vertices[i], filter(lambda v1: v1>=0, regionVertexIndices))
-	boundaryVertices = findBoundaryVertices(p1, ridges, voronoi)
+	boundaryVertices = BOUNDARY.findBoundaryVertices(p1, ridges, voronoi)
 	vertices = np.array(map(lambda v: la.boundPoint(v, particlesim.R_MAX), originalVertices + boundaryVertices))
 	return vertices
-
-def findBoundaryVertices(p1, ridges, voronoi):
-	semiInfiniteRidges = filter(lambda (p2, v1, v2): v1 == -1 or v2 == -1, ridges)
-	boundaryVertices = []
-	for ridge in semiInfiniteRidges:
-		p2, v1, v2 = ridge
-		vertex = la.boundPoint(voronoi.vertices[v1 if v1 >= 0 else v2], particlesim.R_MAX)
-		midpoint = (voronoi.points[p1] + voronoi.points[p2]) / 2
-		endpoint = findIntersectionWithCircle(vertex, midpoint)
-		# testPlot(vertex, voronoi.points[p1], voronoi.points[p2], endpoint)
-		boundaryVertices.append(endpoint)
-	return boundaryVertices
-
-def testPlot(v, x1, x2, endpoint):
-	axes = plt.gca()
-	padding = 1.5
-	axes.set_xlim([-particlesim.R_MAX * padding, particlesim.R_MAX * padding])
-	axes.set_ylim([-particlesim.R_MAX * padding, particlesim.R_MAX * padding])
-	circle = plt.Circle((0, 0), radius=particlesim.R_MAX, color='g', fill=False)
-	axes.add_patch(circle)
-	axes.scatter([endpoint[0]], [endpoint[1]], c='g')
-	axes.scatter([v[0]], [v[1]], c = 'b')
-	axes.scatter([x1[0], x2[0]], [x1[1], x2[1]], c='r')
-	plt.show()
 
 def printVoronoi(cells, voronoi):
 	centroids = np.array(map(lambda cell: cell.centroid, filter(lambda cell: cell.centroid is not None, cells)))
---
+++ voronoiboundary.py
@@ -0,0 +1,70 @@
+import linalgutil as la
+import numpy as np
+import matplotlib.pyplot as plt
+import shapely.geometry as geom
+
+
+
+def testPlot(v, x1, x2, endpoint, rMax):
+	axes = plt.gca()
+	padding = 1.5
+	axes.set_xlim([-rMax * padding, rMax * padding])
+	axes.set_ylim([-rMax * padding, rMax * padding])
+	circle = plt.Circle((0, 0), radius=rMax, color='g', fill=False)
+	axes.add_patch(circle)
+	axes.scatter([endpoint[0]], [endpoint[1]], c='g')
+	axes.scatter([v[0]], [v[1]], c = 'b')
+	axes.scatter([x1[0], x2[0]], [x1[1], x2[1]], c='r')
+	plt.show()
+
+def findIntersectionWithCircle(v, midpoint, rMax):
+	'''
+	We use the geometry of a triangle with points (origin, midpoint, new boundary vertex)
+	to find the vector to the new boundary vertex in the voronoi diagram.
+	create a unit vector pointing from the vertex to the nearest boundary through the midpoint of x1 and x2
+		alpha is the angle between the midpoint vector and the vector from the midpoint to the new boundary vertex
+		beta is the angle between the midpoint/boundary vertex vector and the boundary vertex vector
+		gamma is the angle between the boundary vertex vector and the midpoint vector
+	'''
+	ridgeUnit = (midpoint - v) / np.linalg.norm(midpoint - v)
+	if np.dot(midpoint, ridgeUnit) < 0:
+		ridgeUnit = -ridgeUnit
+	r, theta = la.cartToPolar(v)
+	s, phi = la.cartToPolar(ridgeUnit)
+	alpha = np.pi - theta + phi
+	b = np.linalg.norm(midpoint)
+	a = rMax
+	# Apply the sine rule once to find beta
+	beta = np.arcsin(b * np.sin(alpha) / a)
+	gamma = np.pi - alpha - beta
+	# Apply the sine rule a second time to find the length of the midpoint/boundary vertex vector
+	c = np.sin(gamma) / np.sin(beta) * b
+	# Add the midpiont/boundary vertex vector to the midpoint vector to yield the new vertex vector
+	intersectingPoint = la.polarToCart((c, phi)) + midpoint
+	return intersectingPoint
+
+
+class Circle(object):
+
+	def __init__(self, rMax):
+		self.rMax = float(rMax)
+		self.circle = geom.Point(0,0).buffer(float(rMax)).boundary
+
+	def findBoundaryVertices(self, p1, ridges, voronoi):
+		semiInfiniteRidges = filter(lambda (p2, v1, v2): v1 == -1 or v2 == -1, ridges)
+		boundaryVertices = []
+		for ridge in semiInfiniteRidges:
+			p2, v1, v2 = ridge
+			vertex = la.boundPoint(voronoi.vertices[v1 if v1 >= 0 else v2], self.rMax)
+			midpoint = (voronoi.points[p1] + voronoi.points[p2]) / 2
+			endpoint = findIntersectionWithCircle(vertex, midpoint, self.rMax)
+			# testPlot(vertex, voronoi.points[p1], voronoi.points[p2], endpoint, self.rMax)
+			boundaryVertices.append(endpoint)
+		return boundaryVertices
+
+	def contains(self, x):
+		return np.linalg.norm(x) < self.rMax
+
+	def plot(self, axes):
+		circle = plt.Circle((0,0), radius=self.rMax, color='g', fill=False)
+		axes.add_patch(circle)
