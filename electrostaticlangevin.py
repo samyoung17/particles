7aef28a107434712ccf509622f048bb80ab5d03e
sam <samyoung17a@gmail.com>
2017-12-01 12:26:17 +0000

    Initial commit of Langevin dynamics with local electrostatic repulsion

--- electrostaticlangevin.py
+++ electrostaticlangevin.py
@@ -1,31 +1,42 @@
 import numpy as np
 import particlesim
 import hardboundary
+import linalgutil
 import matplotlib.pyplot as plt
 
 M = 1.0
 GAMMA = 0.1
-S = 0.2
+S = 0.03
 T = 2 * M * pow(S,2) / np.pi
 
-# BOUNDARY = hardboundary.CompactPolygon([(-10.0, -10.0), (-3.0, 2.0), (7.0, 4.0), (9.0, 0.0)])
+R_NEIGHBOUR = 3.0
+Q_TOTAL = 3.0
+
 BOUNDARY = hardboundary.Circle(particlesim.R_MAX)
+
+
+def findNearbyParticleIndices(particles, distances):
+	return filter(lambda j: distances[j] > 0 and distances[j] < R_NEIGHBOUR, range(len(particles)))
 
 def moveParticles(particles, t, boundary):
 	var = 2 * GAMMA * T * t
 	cov = [[var, 0], [0, var]]
 	mean = (0, 0)
 	b = np.random.multivariate_normal(mean, cov, len(particles))
+	D = linalgutil.distanceMatrix(map(lambda p: p.x, particles))
+	q = Q_TOTAL / len(particles)
 	for i, particle in enumerate(particles):
+		jj = findNearbyParticleIndices(particles, D[i])
+		F = sum(map(lambda j: q**2 * (particles[i].x - particles[j].x)/D[i,j]**3, jj))
 		x0, v0 = particle.x, particle.v
-		dv = - (GAMMA/M)*v0*t + (1/M)*b[i]
+		dv = - (GAMMA/M)*v0*t + (F/M)*t + (1/M)*b[i]
 		v = v0 + dv
 		x = x0 + v0 * t
 		x,v = boundary.bounceIfHits(x0, v0, x, v)
 		particle.x, particle.v = x, v
 
 def main():
-	n, iterations = 20, 1000
+	n, iterations = 100, 2000
 	folder = 'data/langevin n={} iter={}'.format(n, iterations)
 	data = particlesim.simulate(iterations, n, moveParticles, folder, BOUNDARY)
 	particlesim.motionAnimation(data, 15, BOUNDARY)
