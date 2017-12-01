4971e00bc101e184a32de702a8ba4eff425297be
sam <samyoung17a@gmail.com>
2017-12-01 12:25:37 +0000

    Circle and quadrilateral same area

--- coverageconfig.py
+++ coverageconfig.py
@@ -13,6 +13,7 @@ import electrostaticboundary
 WIERD_QUADRILATERAL_VERTICES = [(-10.0, -10.0), (-3.0, 2.0), (7.0, 4.0), (9.0, 0.0)]
 RECTANGLE_VERTICES = [(-20.0, -5.0), (-20.0, 5.0), (20.0, 5.0), (20.0, -5.0)]
 SQUARE_VERTICES = [(-10.0, -10.0), (-10.0, 10.0), (10.0, 10.0), (10.0, -10.0)]
+R_MAX = 12.73
 
 def getConfig(n, iter):
 	return [
@@ -20,7 +21,7 @@ def getConfig(n, iter):
 			'name': 'Run and Tumble Circle',
 			'filePath': 'data/run tumble circle n={} iter={}'.format(n, iter),
 			'moveFn': runtumble.moveParticles,
-			'boundary': hardboundary.Circle(particlesim.R_MAX)
+			'boundary': hardboundary.Circle(R_MAX)
 		},
 		{
 			'name': 'Run and Tumble Rectangle',
@@ -28,17 +29,17 @@ def getConfig(n, iter):
 			'moveFn': runtumble.moveParticles,
 			'boundary': hardboundary.CompactPolygon(RECTANGLE_VERTICES)
 		},
-		{
-			'name': 'Run and Tumble Quadrilateral',
-			'filePath': 'data/run tumble Quadrilateral n={} iter={}'.format(n, iter),
-			'moveFn': runtumble.moveParticles,
-			'boundary': hardboundary.CompactPolygon(WIERD_QUADRILATERAL_VERTICES)
-		},
+		# {
+		# 	'name': 'Run and Tumble Quadrilateral',
+		# 	'filePath': 'data/run tumble Quadrilateral n={} iter={}'.format(n, iter),
+		# 	'moveFn': runtumble.moveParticles,
+		# 	'boundary': hardboundary.CompactPolygon(WIERD_QUADRILATERAL_VERTICES)
+		# },
 		{
 			'name': 'Langevin Circle',
 			'filePath': 'data/langevin circle n={} iter={}'.format(n, iter),
 			'moveFn': langevin.moveParticles,
-			'boundary': hardboundary.Circle(particlesim.R_MAX)
+			'boundary': hardboundary.Circle(R_MAX)
 		},
 		{
 			'name': 'Langevin Rectangle',
@@ -46,12 +47,12 @@ def getConfig(n, iter):
 			'moveFn': langevin.moveParticles,
 			'boundary': hardboundary.CompactPolygon(RECTANGLE_VERTICES)
 		},
-		{
-			'name': 'Langevin Quadrilateral',
-			'filePath': 'data/langevin Quadrilateral n={} iter={}'.format(n, iter),
-			'moveFn': langevin.moveParticles,
-			'boundary': hardboundary.CompactPolygon(WIERD_QUADRILATERAL_VERTICES)
-		},
+		# {
+		# 	'name': 'Langevin Quadrilateral',
+		# 	'filePath': 'data/langevin Quadrilateral n={} iter={}'.format(n, iter),
+		# 	'moveFn': langevin.moveParticles,
+		# 	'boundary': hardboundary.CompactPolygon(WIERD_QUADRILATERAL_VERTICES)
+		# },
 		# {
 		# 	'name': 'Metropolis',
 		# 	'filePath': 'data/metropolis n={} iter={}'.format(n, iter),
@@ -66,13 +67,13 @@ def getConfig(n, iter):
 			'name': 'Voronoi circle',
 			'filePath': 'data/voronoi circle n={} iter={}'.format(n, iter),
 			'moveFn': voronoi.moveParticles,
-			'boundary': voronoiboundary.Circle(particlesim.R_MAX)
+			'boundary': voronoiboundary.Circle(R_MAX)
 		},
 		{
 			'name': 'Electrostatic circle',
 			'filePath': 'data/electrostatic circle n={} iter={}'.format(n, iter),
 			'moveFn': electrostaticforce.moveParticles,
-			'boundary': electrostaticboundary.Circle(particlesim.R_MAX)
+			'boundary': electrostaticboundary.Circle(R_MAX)
 		},
 		{
 			'name': 'Electrostatic rectangle',
@@ -80,29 +81,29 @@ def getConfig(n, iter):
 			'moveFn': electrostaticforce.moveParticles,
 			'boundary': electrostaticboundary.CompactPolygon(RECTANGLE_VERTICES)
 		},
-		{
-			'name': 'Electrostatic quadrilateral',
-			'filePath': 'data/electrostatic quadrilateral n={} iter={}'.format(n, iter),
-			'moveFn': electrostaticforce.moveParticles,
-			'boundary': electrostaticboundary.CompactPolygon(WIERD_QUADRILATERAL_VERTICES)
-		},
+		# {
+		# 	'name': 'Electrostatic quadrilateral',
+		# 	'filePath': 'data/electrostatic quadrilateral n={} iter={}'.format(n, iter),
+		# 	'moveFn': electrostaticforce.moveParticles,
+		# 	'boundary': electrostaticboundary.CompactPolygon(WIERD_QUADRILATERAL_VERTICES)
+		# },
 
 		{
 			'name': 'Linear Repulsion Circle',
 			'filePath': 'data/linear repulsion circle n={} iter={}'.format(n, iter),
 			'moveFn': linearrepulsion.moveParticles,
-			'boundary': hardboundary.Circle(particlesim.R_MAX)
+			'boundary': hardboundary.Circle(R_MAX)
 		},
 		{
 			'name': 'Linear Repulsion Rectangle',
 			'filePath': 'data/linear repulsion Rectangle n={} iter={}'.format(n, iter),
 			'moveFn': linearrepulsion.moveParticles,
 			'boundary': hardboundary.CompactPolygon(RECTANGLE_VERTICES)
-		},
-		{
-			'name': 'Linear Repulsion Quadrilateral',
-			'filePath': 'data/linear repulsion Quadrilateral n={} iter={}'.format(n, iter),
-			'moveFn': linearrepulsion.moveParticles,
-			'boundary': hardboundary.CompactPolygon(WIERD_QUADRILATERAL_VERTICES)
 		}
+		# {
+		# 	'name': 'Linear Repulsion Quadrilateral',
+		# 	'filePath': 'data/linear repulsion Quadrilateral n={} iter={}'.format(n, iter),
+		# 	'moveFn': linearrepulsion.moveParticles,
+		# 	'boundary': hardboundary.CompactPolygon(WIERD_QUADRILATERAL_VERTICES)
+		# }
 	]
